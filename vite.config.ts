530fc929a9521dddf5ba7ffa8940c22cb071d5cb
AJIEKCEY <sergheialexei@gmail.com>
2025-09-08 18:59:56 +0300

    sprint-6/step-1 Добавить деплой через gh-pages.

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,10 @@ import path from 'path';
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react()],
-	base: '',
+	base: 'react-burger/',
+	build: {
+		outDir: 'dist',
+	},
 	resolve: {
 		alias: {
 			'@': path.resolve(__dirname, './src'),
